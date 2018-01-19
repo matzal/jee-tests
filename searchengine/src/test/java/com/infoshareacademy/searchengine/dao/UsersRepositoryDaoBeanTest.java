f60793aa722f588e0b040f8719ed0127230b8cd7
matzal <mati.zalewski@gmail.com>
2018-01-19 20:36:46 +0100

    UserRepositoryDaoBean tests

--- searchengine/src/test/java/com/infoshareacademy/searchengine/dao/UsersRepositoryDaoBeanTest.java
+++ searchengine/src/test/java/com/infoshareacademy/searchengine/dao/UsersRepositoryDaoBeanTest.java
@@ -9,8 +9,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,10 +25,14 @@ public class UsersRepositoryDaoBeanTest {
     @Mock
     private UsersRepository usersRepository;
 
+    @Mock
+    private UsersRepositoryDaoRemote usersRepositoryDaoRemote;
+
     @InjectMocks
     UsersRepositoryDao usersRepositoryDao = new UsersRepositoryDaoBean();
 
     private User JANKOWALSKI = newUser("Jan", "Kowalski", "janko", 40, Gender.MAN);
+    private User ANNANOWAK = newUser("Anna", "Nowak", "annno", 30, Gender.WOMAN);
 
     private User newUser(String name, String surname, String login, int age, Gender gender) {
         User user = new User();
@@ -47,17 +56,41 @@ public class UsersRepositoryDaoBeanTest {
 
     @Test
     public void getUserById() {
+        //given
+        when(usersRepository.getUserById(anyInt())).thenReturn(JANKOWALSKI);
+        //when
+        User user = usersRepositoryDao.getUserById(1);
+        //then
+        assertThat(user).isEqualTo(JANKOWALSKI);
     }
 
     @Test
     public void getUserByLogin() {
+        //given
+        when(usersRepository.getUserByLogin("janko")).thenReturn(JANKOWALSKI);
+        //when
+        User user = usersRepositoryDao.getUserByLogin("janko");
+        //then
+        assertThat(user).isEqualTo(JANKOWALSKI);
     }
 
     @Test
     public void getUsersList() {
+        //given
+        when(usersRepository.getUsersList()).thenReturn(Arrays.asList(JANKOWALSKI, ANNANOWAK));
+        //when
+        List<User> userList = usersRepositoryDao.getUsersList();
+        //then
+        assertThat(userList).isEqualTo(Arrays.asList(JANKOWALSKI, ANNANOWAK));
     }
 
     @Test
     public void getUsersNames() {
+        //given
+        when(usersRepositoryDaoRemote.getUsersNames()).thenReturn(Arrays.asList(JANKOWALSKI.getName(), ANNANOWAK.getName()));
+        //when
+        List<String> usersNames = usersRepositoryDaoRemote.getUsersNames();
+        //then
+        assertThat(usersNames).isEqualTo(Arrays.asList(JANKOWALSKI.getName(), ANNANOWAK.getName()));
     }
 }
